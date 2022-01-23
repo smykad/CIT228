4b613594893da837128369345e997936801f96ba
Doug <70054506+smykad@users.noreply.github.com>
2022-01-23 13:18:39 -0500

    commit

--- Lesson1/numberFun.py
+++ Lesson1/numberFun.py
@@ -3,43 +3,40 @@ import random
 def printNum(a_num, a_var):
     print("Number " + str(a_num) + " =", a_var)
 
-def printDiv(var_one, var_two):
-    print(var_one, "/", var_two, "=", var_one/var_two)
-    
-def printFloor(var_one, var_two):
-    print(var_one, "//", var_two, "=", var_one//var_two)
-    
-def printModulus(var_one, var_two):
-    print(var_one, "%", var_two, "=", var_one%var_two)
-    
-def printPlus(var_one, var_two):
-    print(var_one, "+", var_two, "=", var_one+var_two)
-    
-def printMinus(var_one, var_two):
-    print(var_one, "-", var_two, "=", var_one-var_two)
-
-def printMult(var_one, var_two):
-    print(var_one, "*", var_two, "=", var_one*var_two)
+def smoothOperator(var1, var2, operator):
+    if operator == "/":
+        num = var1/var2
+    elif operator == "//":
+        num = var1//var2
+    elif operator == "+":
+        num = var1+var2
+    elif operator == "-":
+        num = var1-var2
+    elif operator == "*":
+        num = var1*var2
+    elif operator == "%":
+        num = var1%var2
+    print(var1, operator, var2, "=", num)
     
 def bothNum():
     printNum(1, num1)
     printNum(2, num2)   
     
-def numFun():
+def numFunTwo():
     bothNum()
-    printDiv(num1, num2)
-    printFloor(num1, num2)
-    printModulus(num1, num2)
-    printPlus(num1, num2)
-    printMinus(num1, num2)
-    printMult(num1, num2)
+    smoothOperator(num1, num2, "/")
+    smoothOperator(num1, num2, "//")
+    smoothOperator(num1, num2, "%")
+    smoothOperator(num1, num2, "+")
+    smoothOperator(num1, num2, "-")
+    smoothOperator(num1, num2, "*")
 
 num1 = random.randrange(1,100)
 num2 = random.randrange(1,100)
 
-numFun()
+numFunTwo()
 
 NUM2 = 25
 num2 = NUM2
 
-numFun()
+numFunTwo()
