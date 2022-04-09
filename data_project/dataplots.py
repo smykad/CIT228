a4a2d42a6b8eea72b701a4ac09de6af0b50fb903
Doug <70054506+smykad@users.noreply.github.com>
2022-04-09 05:55:48 -0400

    commit

--- data_project/dataplots.py
+++ data_project/dataplots.py
@@ -16,6 +16,8 @@ from matplotlib import pyplot as plt
 # 11 Cereals and Grains (FAO (2017)) (kilocalories per person per day)
 # 12 Alcoholic Beverages (FAO (2017)) (kilocalories per person per day)
 
+# food calories are kilocalories
+
 # 0 United States
 # 1 USA 
 
@@ -25,16 +27,24 @@ myFile = 'data_project/data/diet_data.csv'
 with open(myFile) as f:
     reader = csv.reader(f)
 
-    years, codes = [], []
+    years, codes, sugars, alcohols = [], [], [], []
 
     for row in reader:
         match row[1]:
             case 'USA':
                 year = int(row[2])
                 code = row[1]
+                sugar = row[4]
+                alcohol = row[12]
                 years.append(year)
                 codes.append(code)
+                sugars.append(sugar)
+                alcohols.append(alcohol)
 
-print(codes, end=', ')
+print(codes[:5], end=', ')
 print()
-print(years, end=', ')
+print(years[:5], end=', ')
+print()
+print(sugars[:5], end=', ')
+print()
+print(alcohols[:5], end=', ')
