d11f96b018620f95c15be0a0cc634428ae7b3ae8
Doug <70054506+smykad@users.noreply.github.com>
2022-04-16 06:42:59 -0400

    commit

---
+++ data_project/currency.py
@@ -0,0 +1,27 @@
+import csv 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+png = [1,2,3]
+usd = [1]
+pound = [1.31]
+
+barWidth=.25
+
+#position of bar on x axis
+br1 = np.arange(len(usd)) 
+br2 = [x + barWidth for x in br1]
+
+plt.xticks([1],['money']) 
+
+# creating the bar plot 
+plt.bar(br1, usd, color ='lightgreen', width=barWidth, label="USD") 
+plt.bar(br2, pound, color="pink",  width=barWidth, label="Pound")
+  
+plt.ylabel("USD") 
+plt.title("USD vs British Pound", c="blue", fontsize=18) 
+plt.legend(loc="best")
+plt.show()
+
+#EOF
