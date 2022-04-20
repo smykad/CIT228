f15a906dd624dfc8f79df437236ea173e3f62498
smykad <dougsmyka@gmail.com>
2022-04-20 06:26:56 -0400

    commit

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,7 +137,7 @@ LOGIN_URL = 'users:login'
 # Heroku settings.
 import django_heroku
 django_heroku.settings(locals())
-import os
+
 
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
