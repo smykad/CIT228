02d73a5ea70c6c4aa39637e9b20a71cb64168389
smykad <dougsmyka@gmail.com>
2022-04-24 05:37:45 -0400

    commit final project

---
+++ bloggit/bloggits/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
+# Create your views here.
+
+def index(request):
+    """The home page for Bloggit."""
+    return render(request, 'bloggits/index.html')
+
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'bloggits/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'bloggits/topic.html', context)
+
+@login_required
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return redirect('bloggits:topics')
+        
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'bloggits/new_topic.html', context)
+
+@login_required
+def new_entry(request, topic_id):
+    """Add a new entry."""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('bloggits:topic', topic_id=topic.id)
+    
+    # Display a blank or invalid form
+    context = {'topic': topic, 'form': form}
+    return render(request, 'bloggits/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+    
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bloggits:topic', topic_id=topic.id)
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'bloggits/edit_entry.html', context)
